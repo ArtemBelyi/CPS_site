148c9d879806c21846d893c45775abbd2a9e82b2
Artem <abelyi467@gmail.com>
2021-08-03 22:39:53 +0300

    add slider

---
+++ js/slider.js
@@ -0,0 +1,26 @@
+
+const swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+  
+      280: {
+        slidesPerView: 1.20,
+        spaceBetween: 16
+      },
+      
+      528: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      
+      784: {
+        slidesPerView: 3,
+        spaceBetween: 24
+      }
+    }
+  });
